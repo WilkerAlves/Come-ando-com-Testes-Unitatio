3ab880d35fdcca57ed7e02d6874f4e6596cae4eb
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2019-07-31 18:27:48 -0300

    criando o primeiro teste

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -35,10 +35,10 @@ class Avaliador:
         self.maior_lance = sys.float_info.min
         self.menor_lance = sys.float_info.max
 
-    def avalia(self,leilao: Leilao):
+    def avalia(self, leilao: Leilao):
 
         for lance in leilao.lances:
             if lance.valor > self.maior_lance:
                 self.maior_lance = lance.valor
-            elif lance.valor < self.menor_lance:
+            if lance.valor < self.menor_lance:
                 self.menor_lance = lance.valor
