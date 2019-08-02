e829ecc39e0aa3caa0085cef1f1b413de173a632
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2019-08-02 11:54:47 -0300

    utilizando o metodo setup

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -5,20 +5,20 @@ from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 class TestAvaliador(TestCase):
 
+    def setUp(self):
+        self.gui = Usuario('Gui')
+        self.lance_do_gui = Lance(self.gui, 150.0)
+        self.leilao = Leilao('Celular')
+
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
-        gui = Usuario('Gui')
         yuri = Usuario('Yuri')
-
         lance_do_yuri = Lance(yuri, 100.0)
-        lance_do_gui = Lance(gui, 150.0)
 
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance_do_yuri)
-        leilao.lances.append(lance_do_gui)
+        self.leilao.lances.append(lance_do_yuri)
+        self.leilao.lances.append(self.lance_do_gui)
 
         avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        avaliador.avalia(self.leilao)
 
         menor_valor_esperado = 100.0
         maio_valor_esperado = 150.0
@@ -27,19 +27,14 @@ class TestAvaliador(TestCase):
         self.assertEqual(maio_valor_esperado, avaliador.maior_lance)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
-        gui = Usuario('Gui')
         yuri = Usuario('Yuri')
-
-        lance_do_gui = Lance(gui, 150.0)
         lance_do_yuri = Lance(yuri, 100.0)
 
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance_do_yuri)
-        leilao.lances.append(lance_do_gui)
+        self.leilao.lances.append(lance_do_yuri)
+        self.leilao.lances.append(self.lance_do_gui)
 
         avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        avaliador.avalia(self.leilao)
 
         menor_valor_esperado = 100.0
         maio_valor_esperado = 150.0
@@ -49,33 +44,27 @@ class TestAvaliador(TestCase):
 
     def test_deve_retornar_o_mesmo_valor_para_maior_menor_lance_quando_o_leilao_tiver_um_lance(self):
 
-        gui = Usuario('Gui')
-        lance_do_gui = Lance(gui, 150.0)
-        leilao = Leilao('Celular')
-        leilao.lances.append(lance_do_gui)
+        self.leilao.lances.append(self.lance_do_gui)
         avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        avaliador.avalia(self.leilao)
 
         self.assertEqual(150.0, avaliador.maior_lance)
         self.assertEqual(150.0, avaliador.menor_lance)
 
     def test_deve_retornar_o_maior_e_menor_valor_quando_o_leilao_tiver_tres_lances(self):
-        gui = Usuario('Gui')
-        yuri = Usuario('Yuri')
+
         vini = Usuario('Vini')
+        yuri = Usuario('Yuri')
 
-        lance_do_yuri = Lance(yuri, 100.0)
-        lance_do_gui = Lance(gui, 150.0)
         lance_do_vini = Lance(vini, 200.0)
+        lance_do_yuri = Lance(yuri, 100.0)
 
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance_do_yuri)
-        leilao.lances.append(lance_do_gui)
-        leilao.lances.append(lance_do_vini)
+        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.lances.append(lance_do_yuri)
+        self.leilao.lances.append(lance_do_vini)
 
         avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        avaliador.avalia(self.leilao)
 
         menor_valor_esperado = 100.0
         maio_valor_esperado = 200.0
